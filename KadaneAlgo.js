1d00b9b44efeec82a60d33498420c22dad305c4e
Rahul Yadav <34509120+Rahulyadav9@users.noreply.github.com>
2024-11-19 10:24:06 +0530

    Update KadaneAlgo.js

--- KadaneAlgo.js
+++ KadaneAlgo.js
@@ -5,12 +5,19 @@
   Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 */
 const maxSubarraySum = (nums)=>{
-  let sum=0;
-  let maxSum = 0;
-  for(let i=0;i<nums.length;i++){
-     sum+=nums[i];
-     maxSum = Math.max(sum, maxSum);
-     if(sum<0) sum=0;
-  }
-  return maxSum;
+  let max=Number.MIN_SAFE_INTEGER;
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        max=Math.max(sum, max);
+        if(sum<0){
+            sum=0;
+        }
+        //let sum = 0;
+        // for(let j=i;j<nums.length;j++){
+        //     sum+=nums[j];
+        //     max = Math.max(max, sum);
+        // }
+    }
+    return max;
 }
